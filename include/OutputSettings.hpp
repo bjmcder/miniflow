a445b3e3ff5b0be8f2a70d9f44ce4ab92e38455a
Brian J. McDermott <bjmcder@gmail.com>
2021-06-08 05:40:47 -0400

    added docstrings to output settings

--- include/OutputSettings.hpp
+++ include/OutputSettings.hpp
@@ -9,16 +9,28 @@ struct OutputSettings{
     std::string base_name;
     std::string format;
 
-    /**
-     * Default constructor.
-    */
+    /**************************************************************************
+     * Default constructor. Sets default values for Output behavior.
+    **************************************************************************/
     OutputSettings(): write_every(0),
               base_name("tstep"),
               format("binary"){}
 
-    /**
+    /**************************************************************************
      * Preferred constructor. Construct from user-defined output settings.
-    */
+     * 
+     * Parameters
+     * ----------
+     * writeevery : int
+     *  Interval (in timesteps) in which to write output files.
+     * 
+     * basename : std::string
+     *  Base file name for output files. The timestep number is appended to
+     *  the base name.
+     * 
+     * fformat : std::string
+     *  The dataset format (ascii or binary.)
+    **************************************************************************/
     OutputSettings(int writeevery, std::string basename, std::string fformat){
         write_every = writeevery;
         base_name = basename;
